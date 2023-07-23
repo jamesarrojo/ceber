8712af30ce7e10ebcbdb510b9fcea60920e8e0c8
jamesarrojo.01 <jamesarrojo.01@gmail.com>
2023-07-23 19:59:25 +0800

    initial commit - done with setup

---
+++ backend/src/app.ts
@@ -0,0 +1,27 @@
+import 'dotenv/config';
+import express, { NextFunction, Request, Response } from 'express';
+import morgan from 'morgan';
+import createHttpError, { isHttpError } from 'http-errors';
+
+const app = express();
+
+app.use(morgan('dev'));
+
+app.use(express.json());
+
+app.use((req, res, next) => {
+    next(createHttpError(404, 'Endpoint not found'));
+});
+
+app.use((error: unknown, req: Request, res: Response, next: NextFunction) => {
+    console.error(error);
+    let errorMessage = 'An unknown error occured';
+    let statusCode = 500;
+    if (isHttpError(error)) {
+        statusCode = error.status;
+        errorMessage = error.message;
+    }
+    res.status(statusCode).json({ error: errorMessage });
+});
+
+export default app;
